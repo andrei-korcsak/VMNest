528d2bdd90f864cc9f849c0f3e3a4ada10aa6b2d
Andrei <Andrei@DEV>
2025-04-20 23:32:10 -0400

    Add status bubble component and update machine fetching
    Updated `ViewMachinesPage.css` to include styles for a new status bubble component representing machine statuses ("Running" and "Off").
    
    Modified `ViewMachinesPage.jsx` to adjust loading state, change pagination parameters, and sort fetched machines by status. Updated rendering to utilize the new status bubble component for better visual representation.
    
    Removed default value for the `Status` property in `MachineModel.cs` to allow explicit status assignment, enhancing model flexibility.

--- VMNest.Server/Models/MachineModel.cs
+++ VMNest.Server/Models/MachineModel.cs
@@ -11,7 +11,7 @@ public class MachineModel
     public string? Name { get; set; }
     public string? Ip { get; set; }
     public string? MacAddress { get; set; }
-    public string? Status { get; set; } = "alert-warning";
+    public string? Status { get; set; }
     public string? Type { get; set; }
     public bool IsEnabled { get; set; }
 }
